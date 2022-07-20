a9df315a6e6ede02af28945b68b39df3f0aba1e5
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-07-20 23:12:49 +0100

    NamespacedLog: fix crash

--- src/lib/io/NamespacedLog.mjs
+++ src/lib/io/NamespacedLog.mjs
@@ -3,7 +3,20 @@
 import a from './Ansi.mjs';
 import l from './Log.mjs';
 
-import hash_numeric from '../crypto/hash_numeric.mjs';
+/**
+ * Hashes a string to a numerical value.
+ * NOT CRYPTOGRAPHICALLY SECURE!
+ * @param	{string}	str		The string to hash.
+ * @return	{number}	The resulting hashed value as a number.
+ */
+function hash_numeric(str) {
+	let hash = 0;
+	for (let i = 0; i < str.length; i++) {
+		hash = ((hash << 5) - hash) + str.charCodeAt(i);
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+}
 
 class NamespacedLog {
 	constructor(namespace) {
