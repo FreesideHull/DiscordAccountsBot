fc1013fb850fdb8fc8a273e0cf019267f5db65c8
CrimsonTome <crimsontome427@protonmail.com>
2022-07-20 23:01:05 +0100

    start notes for deploying

---
+++ src/lib/sysadmin/pyinfra/deploy.py
@@ -0,0 +1,7 @@
+# TODO
+# ssh to machine - (sorted through ssh config and inventory.py)
+# cd to /home/
+# create x users home directory
+# chown xuser:xuser home directory
+
+# but do that through pyinfra rather than typical bash
