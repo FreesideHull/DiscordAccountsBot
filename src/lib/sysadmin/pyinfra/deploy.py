14c72580b42f8de54fc3820b7b3ba2ac63574925
CrimsonTome <crimsontome427@protonmail.com>
2022-07-20 23:48:30 +0100

    add some actual pyinfra to deploy.py This is not functional out of the box yet. integrations need to be made with other areas such as IPA first. also need to look ino chown in pyinfra

--- src/lib/sysadmin/pyinfra/deploy.py
+++ src/lib/sysadmin/pyinfra/deploy.py
@@ -5,3 +5,22 @@
 # chown xuser:xuser home directory
 
 # but do that through pyinfra rather than typical bash
+
+
+from pyinfra.operations import server, files
+
+username = "test"
+
+server.user(
+    name="Validate user", # or create them if they somehow don't exust
+    user=username,
+    home="/home/"+username,
+)
+files.file(
+    name="Create pyinfra log file",
+    path="/home/"+username"/",
+    user=useraname,
+    group=username,
+    # add `chown` command equivalent here
+    _sudo=True,
+)
