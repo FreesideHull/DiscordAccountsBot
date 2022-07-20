a29e6efafb4840129643fd8a2aec72bd49be3d51
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-07-20 22:34:54 +0100

    fixup

--- src/lib/http/routes.mjs
+++ src/lib/http/routes.mjs
@@ -9,7 +9,7 @@ import middleware_catch_errors from './routes/middleware_errors.mjs';
  * Creates a new router for HTTP requests.
  * @return	{ServerRouter}	The new serve router class instance.
  */
-export default functiondata) {
+export default function() {
 	const router = new ServerRouter();
 	
 	router.on_all(middleware_catch_errors.bind(this, sys.config.verbose));
