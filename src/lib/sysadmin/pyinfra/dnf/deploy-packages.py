7dacc022b0cee51205692ca639a2e942817d690f
CrimsonTome <crimsontome427@protonmail.com>
2022-08-04 23:26:41 +0000

    document package deploy script TODO: add to docs

--- src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
+++ src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
@@ -1,12 +1,11 @@
-from pyinfra.operations import dnf
+from pyinfra.operations import dnf # imports the dnf package from pyinfra
 
-pkgs = input("Enter package names seperated by comma")
-pkgs = pkgs.split(",")
+pkgs = input("Enter package names seperated by comma") # replace with how we get the list of packages
+pkgs = pkgs.split(",") # splits on comma
 
-dnf.packages(
+dnf.packages( # installs/updates the chosen packages
     name="Install selected packages",
     packages=pkgs,
     latest=True,
     _sudo=True,
 )
-
