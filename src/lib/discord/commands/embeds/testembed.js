29cc8deba48844a3f67b135456e47177f9368767
Kieran <kieranrobson1999@gmail.com>
2022-08-04 15:19:27 +0100

    Added disclaimer to embed

--- src/lib/discord/commands/embeds/testembed.js
+++ src/lib/discord/commands/embeds/testembed.js
@@ -14,6 +14,12 @@ module.exports = {
             .setColor('#e4b400')
             .setTitle("Create a Freeside Account!")
             .setDescription(`Press the button below to start the process to create a freeside account!`)
+            .addField('Disclaimer', `
+            This bot requires you to enter your student email and will only accept valid '@hull.ac.uk' emails. If you enter a non-valid email, you will be prompted to try again!\r\n\r\n
+            The email you enter will be used to create your Freeside username.
+
+
+            `)
 
             const row = new discord.MessageActionRow()
             .addComponents(
