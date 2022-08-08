dd1d9ebd528f8a651d47ff35f5c951d0e75fefea
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-08-08 18:02:57 +0100

    deploy-packages: typo

--- src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
+++ src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
@@ -1,6 +1,6 @@
 from pyinfra.operations import dnf # imports the dnf package from pyinfra
 
-pkgs = input("Enter package names seperated by comma") # replace with how we get the list of packages
+pkgs = input("Enter package names separated by comma") # replace with how we get the list of packages
 pkgs = pkgs.split(",") # splits on comma
 
 dnf.packages( # installs/updates the chosen packages
