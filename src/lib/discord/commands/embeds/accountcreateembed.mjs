3efe7f2e73eb22d74de63c9d354381ceb92e8f53
KieranRobson <kieranrobson1999@gmail.com>
2022-08-08 10:33:30 +0000

    discord: indenting in embeds

--- src/lib/discord/commands/embeds/accountcreateembed.mjs
+++ src/lib/discord/commands/embeds/accountcreateembed.mjs
@@ -14,20 +14,20 @@ export default {
     run: async (client, message, args) => {
 			console.log(`DEBUG:embed`, MessageEmbed);
             const embed = new MessageEmbed()
-            .setColor('#e4b400')
-            .setTitle("Create a Freeside Account!")
-            .setDescription(`Press the button below to start the process to create a freeside account!`)
-            .addField('Disclaimer', `
-            This bot requires you to enter your student email and will only accept valid '@hull.ac.uk' emails. If you enter a non-valid email, you will be prompted to try again!\r\n
-            The email you enter will be used to create your Freeside username.
-            `)
+                .setColor('#e4b400')
+                .setTitle("Create a Freeside Account!")
+                .setDescription(`Press the button below to start the process to create a freeside account!`)
+                .addField('Disclaimer', `
+                This bot requires you to enter your student email and will only accept valid '@hull.ac.uk' emails. If you enter a non-valid email, you will be prompted to try again!\r\n
+                The email you enter will be used to create your Freeside username.
+                `)
 
             const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
-                .setCustomId('accountCreate')
-                .setLabel('Create an account')
-                .setStyle('PRIMARY')
+                    .setCustomId('accountCreate')
+                    .setLabel('Create an account')
+                    .setStyle('PRIMARY')
             )
 
         message.channel.send({ embeds: [embed], components: [row] })
