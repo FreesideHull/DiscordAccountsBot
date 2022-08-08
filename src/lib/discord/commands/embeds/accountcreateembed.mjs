c9223e5dd211f87dedf9e8c214d934dc1e13bed1
KieranRobson <kieranrobson1999@gmail.com>
2022-08-08 21:53:40 +0000

    discord: added more buttons to embed

--- src/lib/discord/commands/embeds/accountcreateembed.mjs
+++ src/lib/discord/commands/embeds/accountcreateembed.mjs
@@ -15,8 +15,11 @@ export default {
 			console.log(`DEBUG:embed`, MessageEmbed);
             const embed = new MessageEmbed()
                 .setColor('#e4b400')
-                .setTitle("Create a Freeside Account!")
-                .setDescription(`Press the button below to start the process to create a freeside account!`)
+                .setTitle("Freeside account configuration")
+                .setDescription(`Select a button below to configure your account.`)
+                .addField(`Create Account`, `Create an account and recieve the freeside role!`)
+                .addField(`Link Account`, `Link your current account with discord.`)
+                .addField(`Reset Password`, `Reset the password of the account associated with your discord.`)
                 .addField('Disclaimer', `
                 This bot requires you to enter your student email and will only accept valid '@hull.ac.uk' emails. If you enter a non-valid email, you will be prompted to try again!\r\n
                 The email you enter will be used to create your Freeside username.
@@ -27,8 +30,21 @@ export default {
                 new MessageButton()
                     .setCustomId('accountCreate')
                     .setLabel('Create an account')
-                    .setStyle('PRIMARY')
+                    .setStyle('SUCCESS')
             )
+			.addComponents(
+                new MessageButton()
+                .setLabel('Reset Password')
+                .setStyle('DANGER')
+                .setCustomId('resetPassword')
+            )
+            .addComponents(
+                new MessageButton()
+                .setLabel('Link Account')
+                .setStyle('PRIMARY')
+                .setCustomId('linkAccount')
+            )
+            
 
         message.channel.send({ embeds: [embed], components: [row] })
     },
