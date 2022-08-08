cc94ef05a917cc2febfbe2d6ba2cf95f6b0d78c0
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-08-08 18:19:56 +0100

    Create endpoints forpyinfra calls

--- src/lib/io/run_child_process.mjs
+++ src/lib/io/run_child_process.mjs
@@ -4,8 +4,12 @@ import path from 'path';
 import child_process from 'child_process';
 
 
-function run_child_process(command, args) {
+function run_child_process(command, args, env_vars) {
 	return new Promise((resolve, reject) => {
+		const env = {};
+		Object.assign(env, process.env);
+		Object.assign(env, env_vars); // Environment variables specified by the caller take precedence
+		
 		const executing_child_process = child_process.exec(command, args, (error, stdout, stderr) => {
 			if(error) reject(error);
 			resolve(stdout, stderr);
