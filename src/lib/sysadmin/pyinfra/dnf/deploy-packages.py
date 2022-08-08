993ed506a8b1186ef315c55ce0178a8e9ae9eda1
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2022-08-08 18:27:14 +0100

    pyinfra deploy-packages: use PYINFRA_PACKAGES env var

--- src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
+++ src/lib/sysadmin/pyinfra/dnf/deploy-packages.py
@@ -1,6 +1,7 @@
+import os
 from pyinfra.operations import dnf # imports the dnf package from pyinfra
 
-pkgs = input("Enter package names separated by comma") # replace with how we get the list of packages
+pkgs = os.environ.get("PYINFRA_PACKAGES")
 pkgs = pkgs.split(",") # splits on comma
 
 dnf.packages( # installs/updates the chosen packages
